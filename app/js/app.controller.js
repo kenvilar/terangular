a0851dc6cfc9a72232627896d8bbd606490d563e
Ken Vilar <kenvilar@gmail.com>
2017-06-19 00:13:16 +0800

    App controller

---
+++ app/js/app.controller.js
@@ -0,0 +1,12 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('terangular-controller', [])
+        .controller('MainController', MainController);
+
+    function MainController() {
+        //var vm = this;
+    }
+
+}());
