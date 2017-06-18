4202d7470fb5f42dbe3a0b160c89aeaccbc0728e
Ken Vilar <kenvilar@gmail.com>
2017-06-19 00:16:16 +0800

    about template with controller

---
+++ app/templates/about/about.controller.js
@@ -0,0 +1,13 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('terangular-app')
+        .controller('AboutController', [AboutController]);
+
+    function AboutController() {
+        var vm = this;
+
+        vm.test = "About Us..."
+    }
+}());
